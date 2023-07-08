f179731395766f4c3846e16d34886fad186b8c48
RicardoUy <rvuy62@yahoo.com>
2023-07-07 21:53:50 -0500

    lalala...

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 // write your code here
+
+function renderOneRamen(ramen) {
+ const card = document.createElement('div')
+ card.classname = 'card'
+ 
+ card.innerHTML = ` <img src = "${ramen.image}">`
+ document.querySelector("#ramen-menu").appendChild(card)
+ 
+}
+function getAllRamen(){
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("http:/localhost:3000/ramens")
+    .then(response=> response.json())
+    .then(ramens => ramens.forEach(ramen => renderOneRamen(ramen)))
+
+})
+};
+getAllRamen()
+function getRamenImage() {
+const images = document.querySelectorAll('.ramen-menu');
+images.forEach(image => {
+    image.addEventListener('click', function() {
+    image.src = ramen.image
+    });
+  });
+}
+getRamenImage()
